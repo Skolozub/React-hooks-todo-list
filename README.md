5d573348ff4a9ae5f94d06f372369e34f5313029
Сидоров Константин <sidorovalpha@gmail.com>
2019-09-07 21:33:54 +0300

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# React-hooks-todo-list
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => null;
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
